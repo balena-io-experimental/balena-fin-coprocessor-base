3d74efcc914714ad7c01bb5e02e2fa14b0a8d21e
Alex Bucknall <alex.bucknall@gmail.com>
2019-06-16 18:13:19 +0100

    Testing Power changes to submodule

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "balena.h"
+#include "power.h"
 
 /******************************************************************************
  * @brief  Main function
@@ -8,4 +9,12 @@
 int main(void)
 {
   balenaInit(); // include balena libs
+
+  digitalWrite(12,1);
+  digitalWrite(14,1);
+
+  PowerClass P;
+
+  P.Init(1000, 10000, EM0, EM0);
+  P.PowerDown();
 }
