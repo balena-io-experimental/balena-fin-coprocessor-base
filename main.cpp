35107d1ae6797dc73b9c0f6734b0eadb0d626c13
Alex Bucknall <alex.bucknall@gmail.com>
2019-06-29 15:50:50 +0100

    Changed submodule link

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "balena.h"
-#include "power.h"
 
 /******************************************************************************
  * @brief  Main function
@@ -10,11 +9,4 @@ int main(void)
 {
   balenaInit(); // include balena libs
 
-  digitalWrite(12,1);
-  digitalWrite(14,1);
-
-  PowerClass P;
-
-  P.Init(1000, 10000, EM0, EM0);
-  P.PowerDown();
 }
